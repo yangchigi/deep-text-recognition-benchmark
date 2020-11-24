091dd5d192b65f108ef7aa3bfbb29b2691301c20
Your Name <you@example.com>
2020-11-24 18:10:57 +0900

    v

--- 1.sh
+++ 1.sh
@@ -6,11 +6,13 @@ python3 train.py \
 		--character "핵무기보유와123456자회담철수를선언한미기류北核" \
 		--batch_ratio 0.5-0.5 \
 		--select_data "basic-skew" \
-		--Transformation TPS \
+		--Transformation None \
 		--FeatureExtraction ResNet \
 		--SequenceModeling BiLSTM \
 		--Prediction CTC \
 		--data_filtering_off \
 		--batch_max_length 128 \
-		--workers 8 \
+		--workers 16 \
+		--imgH 32 \
+		--hidden_size 512 \
 		--valInterval 10
